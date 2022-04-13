d680aba0fcf47fcd665f53542ab553e1ac9e0fe2
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-04-13 22:01:00 +0700

    Register Form validation
    80% complete

---
+++ Instakilogram/assets/app.js
@@ -0,0 +1,177 @@
+// FORM VALIDATION
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function FormValidator(formSelection, options) {
+    if (!options) {
+        options = {};
+    }
+    /** @function GetParentElement:  ..... */
+    function GetParentElement(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            // Traverse the outside or new parent.
+            element = element.parentElement;
+        }
+    }
+    /**
+     * Convention create the "RULE"
+     * - If there is an error message --> return "Error Message"
+     * - If there is no error --> return `undefined`;
+     */
+    let validatorRules = {
+        required: (value) => {
+            return value ? undefined : 'Please fill in this field';
+        },
+        email: (value) => {
+            let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Please enter the email';
+        },
+        min: (min) => {
+            return (value) => {
+                return value.length >= min ? undefined : `Please enter at least ${min} characters`;
+            }
+        },
+        max: (max) => {
+            return (value) => {
+                return value.length <= max ? undefined : `Please enter up to ${max} characters`;
+            }
+        },
+        isConfirmed: (value) => {
+            let prevPassword = $('input[type = password]').value
+            return value === prevPassword && prevPassword ? undefined : 'Re-enter password again';
+        },
+        isImage: (value) => {
+            return value ? undefined : 'Please fill this image';
+        }
+    };
+    // Get element in DOM based on "formSelector"
+    const formElement = $(formSelection);
+    var formRules = {};
+    // Handle if there is the existence of form element in the DOM
+    if (formElement) {
+        // Get all inputs tag
+        var inputs = formElement.querySelectorAll('input[name][rules]');
+        // console.log(inputs);
+        // Iterate each input tag
+        for (let input of inputs) {
+            let rules = input.getAttribute('rules').split('|');
+            for (let rule of rules) {
+                var ruleInformation;
+                // If rule has the ':' character --> split ':'
+                var isRuleHasValue = rule.includes(':');
+                if (isRuleHasValue) {
+                    ruleInformation = rule.split(':');
+                    // Get the first value
+                    rule = ruleInformation[0];
+                }
+                // IMPLEMENT THE RULE FUNCTION 
+                let ruleFunction = validatorRules[rule];
+
+                if (isRuleHasValue) {
+                    ruleFunction = ruleFunction(ruleInformation[1]);
+                }
+                if (Array.isArray(formRules[input.name])) {
+                    formRules[input.name].push(ruleFunction);
+                }
+                else {
+                    formRules[input.name] = [ruleFunction];
+                }
+            }
+            // When user blur the input
+            input.onblur = HandleValidator;
+            // When user type the value on the input, the error will be CLEARED
+            input.oninput = HandleClearError;
+        }
+        console.log(formRules);
+
+        function HandleValidator(event) {
+            let rules = formRules[event.target.name];
+            let errorMessage;
+            rules.some((rule, index) => {
+                errorMessage = rule(event.target.value);
+                return errorMessage;
+            })
+            // If there is a message, render it on the UI (User Interface)
+            if (errorMessage) {
+                const formGroup = GetParentElement(event.target, '.form-group');
+                if (formGroup) {
+                    formGroup.classList.add('invalid');
+                    const formMessage = formGroup.querySelector('.form-message');
+                    if (formMessage) {
+                        formMessage.innerText = errorMessage;
+                    }
+                }
+            }
+            return !errorMessage;
+        }
+        function HandleClearError(event) {
+            const formGroup = GetParentElement(event.target, '.form-group');
+            // const inputs = $$('input[name]');
+            if (formGroup.classList.contains('invalid')) {
+                formGroup.classList.remove('invalid');
+                const formMessage = formGroup.querySelector('.form-message');
+                formMessage.innerText = '';
+            }
+        }
+        // ...... FIXED 
+        function changeFontFamily() {
+            const inputs = $$('input[name]');
+            // Change the font-family when users input. 
+            inputs.forEach((input) => {
+                input.oninput = () => {
+                    input.style.fontFamily = 'Poppins, sans-serif';
+                }
+            })
+        }
+    }
+    // Handle the form submitting behavior
+    formElement.onsubmit = (event) => {
+        event.preventDefault();
+        // Confirm the validation of form
+        let inputs = formElement.querySelectorAll('input[name][rules]');
+        let isValid = true;
+        for (let input of inputs) {
+            if (!HandleValidator(
+                {
+                    target: input
+                }
+            )) {
+                isValid = false;
+            }
+        }
+        if (isValid) {
+            if (typeof options.onSubmit === 'function') {
+                let enableInputs = formElement.querySelectorAll('input[name]');
+                let formUserDatabase = Array.from(enableInputs).reduce((values, input, index) => {
+                    switch (input.type) {
+                        case 'file':
+                            values[input.name] = input.files;
+                            break;
+                        default:
+                            values[input.name] = input.value;
+                    }
+                    return values;
+                }, {});
+                // Execute the second paremeter "option" of the main function Validator
+                options.onSubmit(formUserDatabase);
+            }
+            else {
+                formElement.submit();
+            }
+        }
+    }
+}
+
+$("[type=file]").on("change", function () {
+    // Name of file and placeholder
+    var file = this.files[0].name;
+    var dflt = $(this).attr("placeholder");
+    if ($(this).val() != "") {
+        $(this).next().text(file);
+    } else {
+        $(this).next().text(dflt);
+    }
+});
---
+++ Instakilogram/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/animation.css">
+    <link rel="stylesheet" href="./assets/responsive.css">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Instakilogram</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="formBackgroundImage">
+            <!-- BACKGROUND IMAGE -->
+            <?php
+            function img_create($filename, $image_type)
+            {
+                $content = file_get_contents($filename);
+                $base64  = base64_encode($content);
+                return ('data:' . $image_type . ';base64,' . $base64);
+            }
+            ?>
+            <img src="<?php print img_create('./assets/Demo Instakilogram Design/FormBackgroundImage.png', 'image/png'); ?>" style="width: 100%; height: 100%;" alt="Form Background Image" />
+        </div>
+
+        <!-- REGISTER FORM -->
+        <div class="formRegister">
+            <form action="" method="POST" class="form" id="register-form">
+                <h3 class="heading">SIGN UP</h3>
+                <p class="desc">To join the Instakilogram, please fill out the registeration form.</p>
+                <div class="spacer"></div>
+                <div class="form-container">
+                    <div class="form-group">
+                        <!-- SEPARATE THE FIRST AND LAST NAME -->
+                        <div class="sub-form-group">
+                            <!-- FIRST NAME -->
+                            <div class="sub-form-group-input">
+                                <label for="firstName" class="sub-form-label">First name</label>
+                                <input id="firstName" rules="required" name="firstName" type="text" placeholder="Ex: Karma" class="form-control">
+                            </div>
+                            <!-- LAST NAME -->
+                            <div class="sub-form-group-input">
+                                <label for="lastName" class="sub-form-label">Last name</label>
+                                <input id="lastName" rules="required" name="lastName" type="text" placeholder="Ex: Nguyen" class="form-control">
+                            </div>
+                        </div>
+                        <span class="form-message invalid"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="form-label">Email Address</label>
+                        <input id="email" rules="required|email" name="email" type="text" placeholder="Example: email@domain.com" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="form-label">Password</label>
+                        <input id="password" rules="required|min:6" name="password" type="password" placeholder="Enter the password" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation" class="form-label">Retype Password</label>
+                        <input id="password_confirmation" rules="required|isConfirmed" name="password_confirmation" placeholder="Confirm the password" type="password" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="avatar" class="form-label">Profile Picture</label>
+                        <input id="avatar" name="avatar" rules="required|isImage" type="file" class="form-control" placeholder="Profile Picture">
+                        <span class="form-message"></span>
+                    </div>
+                </div>
+                <button class="form-submit">Register</button>
+                <button class="form-clear">Clear</button>
+            </form>
+        </div>
+    </main>
+    <!-- JAVASCRIPT -->
+    <script src="./assets/app.js"></script>
+    <!-- <script src="./assets/file.js"></script> -->
+    <script>
+        FormValidator('#register-form', {
+            onSubmit: function(data) {
+                console.log(data);
+            }
+        });
+    </script>
+</body>
+
+</html>
