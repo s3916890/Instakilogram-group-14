c2597a06676ae4d22a5a91602f87d6bead13c419
karma.2710 <95325806+s3916890@users.noreply.github.com>
2022-04-26 23:17:20 +0700

    Update the homepage (non-login) file

---
+++ template/homepage-non-login.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://use.fontawesome.com/releases/v6.1.1/js/all.js" data-auto-replace-svg="nest"></script>
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/homepage-non-login.css">
+    <title>Instakilogram</title>
+</head>
+
+<body>
+    <div class="homepage-container">
+        <!-- Header of the site -->
+        <header id="header">
+            <nav>
+                <div class="homepage-logo">
+                    <h1>InstaKilogram</h1>
+                </div>
+                <div class="searchBar">
+                    <input type="text" placeholder="Search InstaKilogram..." class="searchInput">
+                    <button id="search-btn" type="submit"><i class="icon-style fa-lg fa-solid fa-magnifying-glass"></i></button>
+                </div>
+
+                <!-- SIGN BUTTON -->
+                <ul class="signBtn">
+                    <li class="btn-item">
+                        <button class="transparent-btn">Sign In</button>
+                    </li>
+                    <li class="btn-item">
+                        <button class="transparent-btn">Sign Up</button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Main content section -->
+        <!-- CONTENT ROW -->
+        <main id="main">
+            <!-- First row -->
+            <div class="post-content">
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+            </div>
+
+            <!-- Second row -->
+            <div class="post-content">
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+            </div>
+
+            <!-- Third row -->
+            <div class="post-content">
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+                <a href="./login.php" class="post">
+                    <div class="author">
+                        <div class="info-container">
+                            <img src="../assets/Demo Instakilogram Design/karma2710.jpeg" class="avatar" alt="avatar">
+                            <h1 class="userName">karma.2710</h1>
+                        </div>
+                        <h2 class="option">Public</h2>
+                    </div>
+                    <img src="../assets/Demo Instakilogram Design/aesthetic.jpg" class="post-image" alt="post image">
+                </a>
+            </div>
+        </main>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <a class="footer-link" href="#">About Us</a>
+            <a class="footer-link" href="#">Privacy</a>
+            <a class="footer-link" href="#">Help</a>
+            &copy; Instakilogram FROM GROUP 14
+        </footer>
+        
+    </div>
+</body>
+
+</html>
