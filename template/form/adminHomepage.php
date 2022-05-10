9052b40e8be2fa7ef7d6f5659640759e37aa78a0
July_the_month <68832820+july-bit@users.noreply.github.com>
2022-05-10 18:41:33 +0700

    Create adminHomepage.php

---
+++ template/form/adminHomepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Homepage</title>
+</head>
+<body>
+    <header>
+        <div id='logo'>Logo</div>
+        <div id='search bar'></div>
+    </header>
+    <main>
+        <div class='allAccounts'>
+            <h2>View information of all user accounts:</h2>
+            <!-- Display a table of users based on user_name and email (optional: image) -->
+        <?php include 'allAccounts.php'?>
+        </div>
+    </main>
+</body>
+</html>
