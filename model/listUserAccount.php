0b7d34e95633fde8b07d72aa07a61d08708d4e8b
July_the_month <68832820+july-bit@users.noreply.github.com>
2022-05-12 22:55:25 +0700

    Update listUserAccount.php

--- model/listUserAccount.php
+++ model/listUserAccount.php
@@ -1,39 +1,23 @@
-<!-- Display a table of users based on user_name and email (optional: image) -->
-<table>
-		<!-- The first row is the tables header -->
-		<tr>
-            <th>Profile Image</th>
-			<th>User ID</th>
-			<th>User Name</th>
-			<th>Email</th>
-			<th>Actions</th>
-		</tr>
-        <!-- Next we will have the template which we will use inside
-	  		the loop and poplulate with the data from the json file. -->
-		<?php
-            $json_data = file_get_contents("../database/account.db");
-            $accounts = json_decode($json_data, true);
-                foreach ($accounts as $account) {
-                    ?>
-                        <tr>
-                            <td>
-                                <!-- Images need to be in the same folder as this file to load -->
-								<img src="<?php echo $account['avatar']; ?>" alt="User Profile Image">
-							</td>
-                            <td>
-                                <?php echo $account['userID']; ?>
-                            </td>
-                            <td>
-                                <?php echo $account['userName']; ?>
-                            </td>
-                            <td>
-                                <?php echo $account['email']; ?>;
-                            </td>
-                            <td>
-                                <input type='button' name='button' value='view'>
-                            </td>
-                        </tr>
-        <?php
-}
+<?php
+    $fileName = "../../database/account.db";
+    $oldRecords = json_decode(file_get_contents($fileName), TRUE);
+    $currentEmailInFileDb = array();
+    $currentRegisterTimeInFileDb = array();
+
+    function user_created_time_cmp($firstPost, $nextPost) {
+        return strtotime($nextPost['registerTime']) - strtotime($firstPost['registerTime']);
+    }
+    uasort($oldRecords, 'user_created_time_cmp');
+    foreach ($oldRecords as $userInputObject) {
+        array_push($currentEmailInFileDb, strtolower($userInputObject['email']));
+        array_push($currentRegisterTimeInFileDb, strtolower($userInputObject['registerTime']));
+    }
+
+
+    $listUserAccounts = array();
+    for($i = 0; $i < sizeof($currentRegisterTimeInFileDb); $i++){
+        array_push($listUserAccounts, $currentEmailInFileDb[$i] . " (" . $currentRegisterTimeInFileDb[$i] . ")");
+    }    
+    $_SESSION["listUserAccounts"] = $listUserAccounts;
 ?>
-</table>
+
