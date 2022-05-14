9cffb26156aafa430b847a95322860f3b508e030
July_the_month <68832820+july-bit@users.noreply.github.com>
2022-05-15 03:14:19 +0700

    Update admin login style

--- view/adminLogin.php
+++ view/adminLogin.php
@@ -4,16 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin Login</title>
+    <!-- Stylesheet CSS -->
+    <link rel="stylesheet" href="../style/animation.css">
+    <link rel="stylesheet" href="../style/responsive.css">
+    <link rel="stylesheet" href="../style/form.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/cookies.css">
 
 </head>
 
 <body>
-    <form method="post" action="../model/adminLogin.php">
-        Username <input type="text" name="admin-account"><br>
-        Password <input type="password" name="admin-password"><br>
-        <input type="submit" name="submit" value="Login">
-    </form>
+<div class="forms">
+        <form method="post" action="../model/adminLogin.php" class="form">
+        <div class="form-container">
+            <!-- USERNAME -->
+            <div class="form-group">
+                <label for="username" class="form-label">Admin Username</label>
+                <input id="userName" rules="required" name="admin-account" type="text" placeholder="Enter Admin's Username" class="form-control">
+                <span class="form-message"></span>
+            </div>
+            <!-- PASSWORD -->
+            <div class="form-group">
+                <label for="password" class="form-label">Admin Password</label>
+                <input id="current-password" name="admin-password" type="password" placeholder="Enter the password" class="form-control">
+                <span class="form-message"></span>
+            </div>
+            <input type="submit" name="submit" value="Login" class="form-submit">
+        </div>
+        </form>
+    </div>
+</div>
 </body>
-
 </html>
+
